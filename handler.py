a1daff2c9935db06395ad2a5e253225e6a240f36
Joao Meyer <1994meyer@gmail.com>
2023-09-24 22:22:27 +0100

    Added a s3guard to avoid big objects

--- handler.py
+++ handler.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+import boto3
 from mangum import Mangum
 
 from readql import app
@@ -13,3 +14,16 @@ def handler(event, context):
     logger.info(json.dumps(event))
     mangum = Mangum(app, lifespan='off')
     return mangum(event, context)
+
+
+def s3guard(event, context):
+    MB_100 = 1024 * 1024 * 100
+    s3 = boto3.client('s3')
+
+    for record in event['Records']:
+        if record['s3']['object']['size'] > MB_100:
+            logger.info('Deleting object %s', record['s3']['object']['key'])
+            s3.delete_object(
+                Bucket=record['s3']['bucket']['name'],
+                Key=record['s3']['object']['key'],
+            )
