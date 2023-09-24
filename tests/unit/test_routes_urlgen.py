ff034bca923979073adb748899112c9d162ded4b
Joao Meyer <1994meyer@gmail.com>
2023-09-24 17:02:35 +0100

    Added tests for urlgen

---
+++ tests/unit/test_routes_urlgen.py
@@ -0,0 +1,20 @@
+import uuid
+from unittest import TestCase
+from unittest.mock import patch, Mock, ANY
+
+from readql.models import FileType
+from readql.routes.urlgen import urlgen
+
+
+class TestRoutesUrlGen(TestCase):
+    def test_urlgen(self):
+        ctx = Mock()
+        ctx.uid = uuid.uuid4()
+        ctx.type = FileType.CSV
+
+        result = urlgen(ctx)
+
+        self.assertDictEqual(result, {'key': f'{ctx.uid}.csv', 'url': ANY})
+
+        ctx.urlgen.generate\
+            .assert_called_once_with(f'{ctx.uid}.csv', seconds=10)
---
+++ tests/unit/test_urlgen.py.py
@@ -0,0 +1,19 @@
+import uuid
+from unittest import TestCase
+from unittest.mock import Mock
+
+from readql.urlgen import UrlGen
+
+
+class TestUrlGen(TestCase):
+    def test_urlgen(self):
+        session = Mock()
+        urlgen = UrlGen(session, 'TEST_BUCKET')
+
+        urlgen.generate('TEST_KEY', 123)
+
+        urlgen.client.generate_presigned_url(
+            Params={'Bucket': 'TEST_BUCKET', 'Key': 'TEST_KEY'},
+            ExpiresIn=123,
+            ClientMethod='put_object',
+        )
