e6b232393a1bf976e0ddbfc45d6681182357cb45
Joao Meyer <1994meyer@gmail.com>
2024-05-05 17:28:33 -0300

    Formatted handelr

--- handler.py
+++ handler.py
@@ -12,18 +12,18 @@ logger.setLevel(logging.INFO)
 
 def handler(event, context):
     logger.info(json.dumps(event))
-    mangum = Mangum(app, lifespan='off')
+    mangum = Mangum(app, lifespan="off")
     return mangum(event, context)
 
 
 def s3guard(event, context):
     MB_100 = 1024 * 1024 * 100
-    s3 = boto3.client('s3')
+    s3 = boto3.client("s3")
 
-    for record in event['Records']:
-        if record['s3']['object']['size'] > MB_100:
-            logger.info('Deleting object %s', record['s3']['object']['key'])
+    for record in event["Records"]:
+        if record["s3"]["object"]["size"] > MB_100:
+            logger.info("Deleting object %s", record["s3"]["object"]["key"])
             s3.delete_object(
-                Bucket=record['s3']['bucket']['name'],
-                Key=record['s3']['object']['key'],
+                Bucket=record["s3"]["bucket"]["name"],
+                Key=record["s3"]["object"]["key"],
             )
